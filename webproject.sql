69e4ff1bd3df475fedf9bee9927fe34b00f9768d
cjoc40 <113445389+cjoc40@users.noreply.github.com>
2023-11-24 22:25:29 +0000

    24th November

--- webproject.sql
+++ webproject.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Nov 12, 2023 at 04:07 PM
+-- Generation Time: Nov 24, 2023 at 09:28 PM
 -- Server version: 10.4.22-MariaDB
 -- PHP Version: 7.4.27
 
@@ -24,23 +24,36 @@ SET time_zone = "+00:00";
 -- --------------------------------------------------------
 
 --
+-- Table structure for table `cart`
+--
+
+CREATE TABLE `cart` (
+  `id` int(11) NOT NULL,
+  `session_id` varchar(255) NOT NULL,
+  `product_id` int(11) NOT NULL,
+  `quantity` int(11) NOT NULL,
+  `created_at` timestamp NOT NULL DEFAULT current_timestamp(),
+  `updated_at` timestamp NOT NULL DEFAULT current_timestamp() ON UPDATE current_timestamp()
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+-- --------------------------------------------------------
+
+--
 -- Table structure for table `category`
 --
 
 CREATE TABLE `category` (
-  `categoryID` varchar(10) NOT NULL,
-  `productID` varchar(10) DEFAULT NULL,
-  `categoryName` varchar(255) DEFAULT NULL,
-  `description` varchar(255) DEFAULT NULL,
-  `picture` varchar(255) DEFAULT NULL
+  `categoryID` int(11) NOT NULL,
+  `categoryName` varchar(255) DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
 --
 -- Dumping data for table `category`
 --
 
-INSERT INTO `category` (`categoryID`, `productID`, `categoryName`, `description`, `picture`) VALUES
-('C2', 'P2', 'Electric Guitars', 'LA Electric Guitar', '[LA_electricGuitar.png]');
+INSERT INTO `category` (`categoryID`, `categoryName`) VALUES
+(1, 'Mens'),
+(2, 'Womens');
 
 -- --------------------------------------------------------
 
@@ -66,7 +79,7 @@ CREATE TABLE `member` (
 --
 
 INSERT INTO `member` (`memberID`, `firstName`, `lastName`, `email`, `phoneNum`, `passWord`, `address1`, `address2`, `town_city`, `county`) VALUES
-(1, 'Kristers', 'Rakstins', 'kristerssrakstinss@gmail.com', '(+353 89 982 7826)', '**********', '10 Convent Lane', 'Drangan', 'Thurles', 'Tipperary');
+(1, 'Kristers', 'Rakstins', 'kristerssrakstinss@gmail.com', '0899827826', '$2y$10$FJgWX.oxBveS.QF5IkfxjO5R4LImqnX8yzQkYhda0z4hpUnwREyW.', '10 Convent Lane', 'Drangan', 'Thurles', 'Tipperary');
 
 -- --------------------------------------------------------
 
@@ -120,16 +133,16 @@ INSERT INTO `orders` (`orderID`, `productID`, `memberID`, `quantity`, `orderDate
 --
 
 CREATE TABLE `products` (
-  `productID` varchar(10) NOT NULL,
-  `categoryID` varchar(10) DEFAULT NULL,
-  `warehouseID` varchar(10) DEFAULT NULL,
-  `supplierID` varchar(10) DEFAULT NULL,
-  `barcode` varchar(20) DEFAULT NULL,
+  `productID` int(11) NOT NULL,
+  `categoryID` varchar(50) DEFAULT NULL,
+  `warehouseID` varchar(50) DEFAULT NULL,
+  `supplierID` varchar(50) DEFAULT NULL,
+  `barcode` varchar(50) DEFAULT NULL,
   `brand` varchar(255) DEFAULT NULL,
-  `productDesc` varchar(255) DEFAULT NULL,
-  `price` varchar(10) DEFAULT NULL,
+  `productDesc` text DEFAULT NULL,
+  `price` decimal(10,2) DEFAULT NULL,
   `quantity` int(11) DEFAULT NULL,
-  `productAvailability` tinyint(1) DEFAULT NULL
+  `productAvailability` int(11) DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
 --
@@ -137,7 +150,7 @@ CREATE TABLE `products` (
 --
 
 INSERT INTO `products` (`productID`, `categoryID`, `warehouseID`, `supplierID`, `barcode`, `brand`, `productDesc`, `price`, `quantity`, `productAvailability`) VALUES
-('P2', 'C2', 'W3', 'S2', '{5012345678900}', 'Nike', 'LA Electric Guitar', '€150', 1, 1);
+(1, '1', 'W2', 'S2', '{5012345678922}', 'Adidas', 'LA Electric Guitar', '55.00', 1, 1);
 
 -- --------------------------------------------------------
 
@@ -194,6 +207,27 @@ INSERT INTO `suppliers` (`supplierID`, `companyName`, `contactName`, `contactTit
 -- --------------------------------------------------------
 
 --
+-- Table structure for table `users`
+--
+
+CREATE TABLE `users` (
+  `id` int(11) NOT NULL,
+  `name` varchar(25) DEFAULT NULL,
+  `username` varchar(20) DEFAULT NULL,
+  `password` varchar(255) DEFAULT NULL,
+  `email` varchar(40) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data for table `users`
+--
+
+INSERT INTO `users` (`id`, `name`, `username`, `password`, `email`) VALUES
+(4, 'Kristers', 'Kristers123', '$2y$10$mJD1idPgu7Cz3lt.ZNsNYezNXUDdCo.QVyo79g1hnewc6EgzYszWi', 'admin@gmail.com');
+
+-- --------------------------------------------------------
+
+--
 -- Table structure for table `warehouse`
 --
 
@@ -220,6 +254,13 @@ INSERT INTO `warehouse` (`warehouseID`, `warehouseName`, `address1`, `address2`,
 --
 -- Indexes for dumped tables
 --
+
+--
+-- Indexes for table `cart`
+--
+ALTER TABLE `cart`
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `product_id` (`product_id`);
 
 --
 -- Indexes for table `category`
@@ -264,10 +305,61 @@ ALTER TABLE `suppliers`
   ADD PRIMARY KEY (`supplierID`);
 
 --
+-- Indexes for table `users`
+--
+ALTER TABLE `users`
+  ADD PRIMARY KEY (`id`),
+  ADD UNIQUE KEY `email` (`email`);
+
+--
 -- Indexes for table `warehouse`
 --
 ALTER TABLE `warehouse`
   ADD PRIMARY KEY (`warehouseID`);
+
+--
+-- AUTO_INCREMENT for dumped tables
+--
+
+--
+-- AUTO_INCREMENT for table `cart`
+--
+ALTER TABLE `cart`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
+
+--
+-- AUTO_INCREMENT for table `category`
+--
+ALTER TABLE `category`
+  MODIFY `categoryID` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+
+--
+-- AUTO_INCREMENT for table `member`
+--
+ALTER TABLE `member`
+  MODIFY `memberID` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+
+--
+-- AUTO_INCREMENT for table `products`
+--
+ALTER TABLE `products`
+  MODIFY `productID` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+
+--
+-- AUTO_INCREMENT for table `users`
+--
+ALTER TABLE `users`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
+
+--
+-- Constraints for dumped tables
+--
+
+--
+-- Constraints for table `cart`
+--
+ALTER TABLE `cart`
+  ADD CONSTRAINT `cart_ibfk_1` FOREIGN KEY (`product_id`) REFERENCES `products` (`productID`);
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
